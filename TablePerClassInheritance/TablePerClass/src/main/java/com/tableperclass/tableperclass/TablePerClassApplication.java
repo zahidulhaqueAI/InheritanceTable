5fd01dd59071a8a1e4ec8621fabf8c2487d50ff3
Zahid <zahidulhaquestar@gmail.com>
2024-08-15 23:29:39 +0530

    Table Per Class initial commit

---
+++ TablePerClassInheritance/TablePerClass/src/main/java/com/tableperclass/tableperclass/TablePerClassApplication.java
@@ -0,0 +1,13 @@
+package com.tableperclass.tableperclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablePerClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TablePerClassApplication.class, args);
+    }
+
+}
---
+++ TablePerClassInheritance/TablePerClass/src/test/java/com/tableperclass/tableperclass/TablePerClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tableperclass.tableperclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablePerClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
